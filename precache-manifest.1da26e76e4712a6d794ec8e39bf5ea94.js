f3beee3f016749b40b7674a5e5191cdf5edef87a
Hamish McBrearty <Hamish.McBrearty@concepts.co.nz>
2019-05-10 11:09:11 +1200

    Updates

---
+++ precache-manifest.1da26e76e4712a6d794ec8e39bf5ea94.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7f041da7dd854d9efbd8e0a5d9aafda",
+    "url": "/rollordie/index.html"
+  },
+  {
+    "revision": "531f08fb20afcd2db3fd",
+    "url": "/rollordie/static/css/main.feccc708.chunk.css"
+  },
+  {
+    "revision": "41dd3d2743248b211c35",
+    "url": "/rollordie/static/js/2.24465e23.chunk.js"
+  },
+  {
+    "revision": "531f08fb20afcd2db3fd",
+    "url": "/rollordie/static/js/main.92ee6a02.chunk.js"
+  },
+  {
+    "revision": "c797df47054697abcc88",
+    "url": "/rollordie/static/js/runtime~main.fc2b89a5.js"
+  }
+]);
